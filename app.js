b8e87f909126decb791a0371567b0d50db7009a8
rbtanna2321 <46971991+rbtanna2321@users.noreply.github.com>
2019-06-21 14:49:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var moment = require('moment');
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/views'));
+global.__basedir = __dirname;
+
+// Configuring the database
+const dbConfig = require('./app/config/mongodb.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url)
+.then(() => {
+    console.log("Successfully connected to MongoDB.");
+}).catch(err => {
+    console.log('Could not connect to MongoDB.');
+    process.exit();
+});
+
+require('./app/routes/user.route.js')(app);
+
+// Create a Server
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("App listening at http://%s:%s", host, port)
+
+});
+
